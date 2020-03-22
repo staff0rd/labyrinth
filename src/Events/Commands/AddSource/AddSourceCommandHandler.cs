81e6e9a63d2726b2592205e9e1633a3c5d29ab03
Stafford Williams <stafford.williams@gmail.com>
2020-03-22 20:07:56 +1100

    dupe check

--- src/Events/Commands/AddSource/AddSourceCommandHandler.cs
+++ src/Events/Commands/AddSource/AddSourceCommandHandler.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -15,7 +16,15 @@ namespace Events
 
         public async Task<Result> Handle(AddSourceCommand request, CancellationToken cancellationToken)
         {
-            await _sources.Add(new Credential { Username = request.Username, Password = request.Password }, request.Id, request.Network, request.Name);
+            var creds = new Credential { Username = request.Username, Password = request.Password };
+            var sources = await _sources.Get(creds);
+            
+            if (sources.Any(s => s.Network == request.Network && s.Name == request.Name))
+            {
+                return Result.Error($"A source called {request.Name} already exists for ${request.Network}");
+            }
+
+            await _sources.Add(creds, request.Id, request.Network, request.Name);
             return Result.Ok();
         }
     }
