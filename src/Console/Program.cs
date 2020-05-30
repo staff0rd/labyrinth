1462cad95eb427091472f63ce50b78ab66da82be
Stafford Williams <stafford.williams@gmail.com>
2020-05-30 23:37:52 +1000

    cleanup

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using McMaster.Extensions.CommandLineUtils;
-using System.Collections.Generic;
-using System.Linq;
 using Console;
 using Events;
 using MediatR;
@@ -10,18 +7,17 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 
-namespace CustomServices
+namespace Console
 {
-   
-    [HelpOption]
     class Program
     {
         static async Task Main(string[] args)
         {
-            await CreateHostBuilder()
-                .RunCommandLineApplicationAsync(args, (app) => {
-                    new RegisterCommands(typeof(ChangePasswordCommand).Assembly).Apply(app);
-                });
+            await CreateHostBuilder().RunCommandLineApplicationAsync(args, (app) => {
+                new RegisterCommands(typeof(ChangePasswordCommand).Assembly).Apply(app);
+                if (args.Length == 0)
+                    app.ShowHelp();
+            });
         }
 
         public static IHostBuilder CreateHostBuilder()
@@ -30,7 +26,6 @@ namespace CustomServices
                 .ConfigureServices((hostContext, services) =>
                 {
                     var connectionString = hostContext.Configuration.GetConnectionString("EventsConnection");
-
                     services
                         .AddSingleton<KeyRepository>()
                         .AddMediatR(typeof(ChangePasswordCommand).Assembly)
@@ -38,11 +33,6 @@ namespace CustomServices
                             return new NpgsqlConnectionFactory(connectionString);
                         });
                 });
-        }
-
-        private void OnExecute()
-        {
-            System.Console.WriteLine("Nothing");
         }
     }
 }
