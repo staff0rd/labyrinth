e9fe75a8a3dcdf010eeefbce9392bb9f010de9b5
Stafford Williams <stafford.williams@gmail.com>
2020-05-30 23:16:02 +1000

    create account console

--- src/Events/Commands/CreateAccount/CreateAccountCommand.cs
+++ src/Events/Commands/CreateAccount/CreateAccountCommand.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using McMaster.Extensions.CommandLineUtils;
 using MediatR;
 
 namespace Events 
 {
-    public class CreateAccountCommand : IRequest<Result>
+    [Command(Name="create-account", Description= "Create a new account")]
+    public class CreateAccountCommand : ResultCommand
     {
+        [Required]
+        [Option("-u|--username", CommandOptionType.SingleValue, Description = "User name")]
         public string UserName { get; set; }
-
+        
+        [Required]
+        [Option("-p|--password", CommandOptionType.SingleValue, Description = "Password")]
         public string Password { get; set; }
     }
 }
