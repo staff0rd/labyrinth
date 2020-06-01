a91a2d4dd4e14af60e5e1389bd359f9c929291f3
Stafford Williams <stafford.williams@gmail.com>
2020-06-01 21:18:12 +1000

    get-sources

--- src/Events/Commands/ChangePassword/ResultCommand.cs
+++ src/Events/Commands/ChangePassword/ResultCommand.cs
@@ -5,6 +5,27 @@ using System;
 
 namespace Events
 {
+    public abstract class ResultCommand<T> : IRequest<Result<T>>
+    {
+        protected abstract void DisplayResult(T result);
+
+        protected async Task OnExecuteAsync(IMediator mediator, ILogger<ResultCommand> logger)
+        {
+            try
+            {
+                var result = await mediator.Send(this);
+                if (result.IsError)
+                    logger.LogError(result.Message);
+                else
+                    DisplayResult(result.Response);
+            } 
+            catch (Exception e)
+            {
+                logger.LogError(e, "Failed to execute command");
+            }
+        }
+    }
+
     public abstract class ResultCommand : IRequest<Result>
     {
         protected async Task OnExecuteAsync(IMediator mediator, ILogger<ResultCommand> logger)
--- src/Events/Queries/GetSources/GetSourcesQuery.cs
+++ src/Events/Queries/GetSources/GetSourcesQuery.cs
@@ -1,11 +1,28 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
 using MediatR;
+using ConsoleTables;
 
 namespace Events
 {
-    public class GetSourcesQuery : IRequest<Result<List<Source>>>
+    [Command(Name="get-sources", Description="Get the list of sources currently configured")]
+    public class GetSourcesQuery : ResultCommand<List<Source>>
     {
-        public string Username {get; set;}
-        public string Password { get; set;}
+        [Required]
+        [Option("-u|--username", CommandOptionType.SingleValue, Description = "User name")]
+        public string Username { get; set; }
+        
+        [Required]
+        [Option("-p|--password", CommandOptionType.SingleValue, Description = "Password")]
+        public string Password { get; set; }
+
+        protected override void DisplayResult(List<Source> result)
+        {
+            ConsoleTable
+                .From(result)
+                .Write(Format.Minimal);
+        }
     }
 }
