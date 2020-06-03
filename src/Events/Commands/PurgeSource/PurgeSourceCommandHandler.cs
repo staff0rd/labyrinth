738a1e6bd812f3165f77c9d7248980b6696eab23
Stafford Williams <stafford.williams@gmail.com>
2020-06-03 21:37:36 +1000

    remove namespace

--- src/Events/Commands/PurgeSource/PurgeSourceCommandHandler.cs
+++ src/Events/Commands/PurgeSource/PurgeSourceCommandHandler.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace Events
