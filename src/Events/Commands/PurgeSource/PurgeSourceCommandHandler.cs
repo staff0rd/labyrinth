d2e2e0f055638c028cc98dfec35ecf7003d07fc9
Stafford Williams <stafford.williams@gmail.com>
2020-06-03 21:36:46 +1000

    delete images

--- src/Events/Commands/PurgeSource/PurgeSourceCommandHandler.cs
+++ src/Events/Commands/PurgeSource/PurgeSourceCommandHandler.cs
@@ -1,22 +1,35 @@
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
 
 namespace Events
 {
     public class PurgeSourceCommandHandler : IRequestHandler<PurgeSourceCommand, Result>
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<PurgeSourceCommandHandler> _logger;
 
-        public PurgeSourceCommandHandler(IMediator mediator)
+        public PurgeSourceCommandHandler(IMediator mediator, ILogger<PurgeSourceCommandHandler> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         public async Task<Result> Handle(PurgeSourceCommand request, CancellationToken cancellationToken)
         {
-            return await _mediator.Send(new PurgeEventsCommand { Username = request.Username, Password = request.Password, Events = new string[0], SourceId = request.SourceId });
+            var result = await _mediator.Send(new PurgeEventsCommand { Username = request.Username, Password = request.Password, Events = new string[0], SourceId = request.SourceId });
+            if (!result.IsError)
+            {
+                _logger.LogInformation("Deleting images...");
+                var dir = TeamsBackfillCommandHandler.GetImageDirectory(request.SourceId);
+                Directory.Delete(dir, true);
+                _logger.LogInformation("Images deleted");
+            }
+            return result;
         }
     }
 }
